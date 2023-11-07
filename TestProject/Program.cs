7e0e81361e1d23b5d02db9a6916f4633709fe2c0
Samuel Osho <samthatcode@gmail.com>
2023-11-07 07:21:39 +0000

    First Commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = {4, 8, 15, 16, 23, 42 };
+int total = 0;  // Initialize 'total' to zero outside the loop
+bool found = false;  // Initialize 'found' to false outside the loop
+
+foreach (int number in numbers)
+{
+   
+    total += number;
+
+    if (number == 42)
+    {
+         found = true;
+    }
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+}
+Console.WriteLine($"Total: {total}");
